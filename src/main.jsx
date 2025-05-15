8425a01c1d562e8055c70b666d8cbcc45bb0f2bb
franchierchie <franchierchie@gmail.com>
2025-05-15 16:34:08 -0300

    fixed EcommerceApp path in main.jsx & README modified

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import { store } from './store';
-import { EcommerceApp } from './ecommerceApp';
+import { EcommerceApp } from './EcommerceApp';
 
 import './index.css';
 
